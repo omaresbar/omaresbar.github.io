e6f61bdf5f7c54c7bcfe33394950b10630986e25
unknown <omare.b@hotmail.com>
2017-04-08 10:40:38 -0500

    some useful comment

---
+++ jsexamples/jscripts.js
@@ -0,0 +1,20 @@
+function msg() {
+    var val1= 
+   Number(document.getElementById("textbox1").value);
+    var val2=
+   Number(document.getElementById("textbox2").value);
+     var val3= 
+   Number(document.getElementById("textbox3").value);
+    var val4=
+   Number(document.getElementById("textbox4").value);
+     var val5= 
+   Number(document.getElementById("textbox5").value);
+    var val6=
+   Number(document.getElementById("textbox6").value);
+     var val7= 
+   Number(document.getElementById("textbox7").value);
+    var val8=
+   Number(document.getElementById("textbox8").value);
+    var sum = val1 +val2 + val3 + val4 +val5 + val6 + val7 + val8;
+         alert(sum);
+}
